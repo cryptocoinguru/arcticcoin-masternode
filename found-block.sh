0c2379f4d589d5bc8d3d2eb72f36b19be5681811
cryptocoinguru <36725063+cryptocoinguru@users.noreply.github.com>
2019-10-01 20:23:52 +0200

    Create found-block.sh

---
+++ found-block.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+_walletDir=$(dirname $0);
+
+if [[ ! -z $@ ]];
+then
+  echo "$@" >> ${_walletDir}/new-block.notify;
+
+  # Truncate log-file to 1000 lines to keep it small
+  tail -n 1000 ${_walletDir}/new-block.notify > ${_walletDir}/new-block.notify.tmp;
+
+  if [[ -f ${_walletDir}/new-block.notify.tmp ]];
+  then
+    mv ${_walletDir}/new-block.notify.tmp ${_walletDir}/new-block.notify;
+  fi
+fi
+
+${_walletDir}/watchdog.sh --full-scan;
